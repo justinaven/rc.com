0e6a5addff86a71431e45d7a30cefc9555f0e44c
javen <justin.aven@lifeway.com>
2017-04-15 21:38:57 -0500

    latest changes

--- js/global.js
+++ js/global.js
@@ -9,6 +9,15 @@ Prism.languages.javascript=Prism.languages.extend("clike",{keyword:/\b(as|async|
 
 Zepto(function($){
   $('html').addClass('js');
+  $('.lineup__row .film').addClass('closed');
+
+  // Film Summary toggle
+  $('.lineup__row').each(function(index){
+    $(this).find('.film .flim__link').on('click', function(e){
+      e.preventDefault();
+      $(this).closest('.film').toggleClass('closed');
+    });
+  });
 
   // initialize all modals & modal-links on page load
   if($('.modal-link').length>0){
@@ -24,11 +33,15 @@ Zepto(function($){
     function openModal(target_id){
       $('html').addClass('modal-on');
       $(target_id).addClass('on');
+      console.log($(target_id).closest('.film.inner.closed'));
+      if($(target_id).closest('.film.inner.closed').length > 0){
+        $(target_id).closest('.lineup__row').addClass('has-modal-open');
+      }
     }
     // close modal if anywhere other than modal is clicked or if ESC key pressed
     $('.modal__container, .modal__container .modal__wrapper, .modal__container .modal__close').on('click', function(){
       $('html').removeClass('modal-on');
-      $(this).removeClass('on').find('.modal__container.on').removeClass('on');
+      $(this).removeClass('on').find('.has-modal-open').removeClass('has-modal-open').find('.modal__container.on').removeClass('on');
     });
     $(document).keyup(function(e) {
       if (e.keyCode == 27) { $('.modal__container.on').removeClass('on'); }
